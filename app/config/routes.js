0daf41e17d65c7539370df1ff3a5e94af233c80c
1312071 <1312071@student.hcmus.edu.vn>
2017-06-17 20:08:12 +0700

    categories-management

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,4 +1,5 @@
 var aboutController = require('../controllers/aboutController');
+var categoriesController = require('../controllers/categoriesController');
 
 var configRoutes = function(app){
   app.get('/', function(req, res){
@@ -8,6 +9,12 @@ var configRoutes = function(app){
   app.get('/admin', function(req, res){
     res.render('admin/dashboard/dashboard');
   });
+  app.get('/admin/categories', categoriesController.index);
+  app.post('/admin/categories', categoriesController.create);
+  app.delete('/admin/categories/:id', categoriesController.delete);
+  app.get('/admin/categories/:id/edit', categoriesController.getCategory);
+  app.post('/admin/categories/:id/edit', categoriesController.edit);
+  app.get('/admin/categories/:search_string', categoriesController.category_search);
 };
 
 module.exports = configRoutes;
