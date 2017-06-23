bd02e3d659aa141b05bc7a5d5253a55551322c31
Mango95 <nmhai1995@gmail.com>
2017-06-24 01:32:27 +0700

    Add login-with-FB option

--- app/controllers/aboutController.js
+++ app/controllers/aboutController.js
@@ -4,10 +4,10 @@ var aboutController = {
     var url = req.protocol + '://' + req.get('host');
     categories.index(function(categories){
       if(categories){
-        res.render('about', {categories: categories.rows, url: url});
+        res.render('about', {categories: categories.rows, url: url, user: req.user});
       }
       else{
-        res.render('about', {categories: '', url: url});
+        res.render('about', {categories: '', url: url, user: req.user});
       }
     })
   }
